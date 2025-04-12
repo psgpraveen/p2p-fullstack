076abdb64f34e4750d4ff455f625bf62f9373510
Praveen Kumar Gupta <pg23403@gmail.com>
2025-04-12 10:14:36 +0530

    jhf

--- p2p-book-exchange-backend/server.js
+++ p2p-book-exchange-backend/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -21,7 +22,9 @@ app.get('/', (req, res) => {
 });
 
 // Start server
-const PORT = 5000;
+const PORT = process.env.PORT  || 5000;
+console.log(PORT);
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
